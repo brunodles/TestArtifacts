ed7e03cc2619276837cd6d1b27d39bfbc34a23b1
Bruno de Lima <dlimaun@gmail.com>
2018-01-22 01:04:55 -0200

    Prepare test for task MergeTestArtifacts

--- src/test/groovy/com/brunodles/testartifacts/task/MergeTestArtifactsTest.groovy
+++ src/test/groovy/com/brunodles/testartifacts/task/MergeTestArtifactsTest.groovy
@@ -1,41 +1,55 @@
 package com.brunodles.testartifacts.task
 
-import com.brunodles.testartifacts.TestArtifactsExtension
 import com.brunodles.testartifacts.TestArtifactsPlugin
 import org.gradle.api.Project
-import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.GradleRunner
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 @RunWith(JUnit4.class)
 class MergeTestArtifactsTest {
 
-    private Project project
+    @Rule
+    public TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
 
     @Before
     void setupProject() {
-        project = ProjectBuilder.builder().build()
-        project.pluginManager.apply TestArtifactsPlugin
-        (project.extensions.archiver as TestArtifactsExtension).with {
-            projectName = "AnimeWatcher"
-            moduleName = "Explorer"
-            buildNumber = 123
-            files = [
-                    'checkstyle': ['ktlint.xml', 'reports/checkstyle/main.xml'],
-                    'jacoco'    : ['reports/jacoco/test/jacocoTestReport.xml'],
-                    'jacocoCsv' : ['reports/jacoco/test/jacocoTestReport.csv'],
-                    'lint'      : ['lint-results.xml'],
-                    'test'      : ['test-results/test/TEST**.xml']
-            ]
-        }
+        buildFile = testProjectDir.newFile('build.gradle')
+        buildFile << """
+            plugins {
+                id 'testartifacts' version '0.1.0'
+            }
+            archiver {
+                projectName = "AnimeWatcher"
+                moduleName = "Explorer"
+                buildNumber = 123
+                files = [
+                        'checkstyle': ['ktlint.xml', 'reports/checkstyle/main.xml'],
+                        'jacoco'    : ['reports/jacoco/test/jacocoTestReport.xml'],
+                        'jacocoCsv' : ['reports/jacoco/test/jacocoTestReport.csv'],
+                        'lint'      : ['lint-results.xml'],
+                        'test'      : ['test-results/test/TEST**.xml']
+                ]
+            }
+        """
     }
 
     @Test
-    void withoutSetup_extensionShouldBeEmpty() {
-        def archiver = project.extensions.archiver
-        Assert.fail("WIP")
+    void shouldCreateMergeReportFile() {
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('mergeTestArtifacts')
+                .withPluginClasspath()
+                .build()
+        Assert.assertTrue(result.output.contains("Saving at"))
+        Assert.assertTrue(result.task(":mergeTestArtifacts").outcome == SUCCESS)
     }
 }

049586c0ed17ebc3954dfa442b70a99c1586af72
Bruno de Lima <dlimaun@gmail.com>
2018-01-23 08:28:38 -0200

    Add new assertMatches function to Assertions

--- src/test/groovy/com/brunodles/testing/Assertions.groovy
+++ src/test/groovy/com/brunodles/testing/Assertions.groovy
@@ -8,6 +8,10 @@ final class Assertions {
     private Assertions() {
     }
 
+    static void assertMatches(String pattern, String result) {
+        assertMatches(Pattern.compile(pattern), result)
+    }
+
     static void assertMatches(Pattern pattern, String result) {
         Assert.assertTrue("Expected: ${pattern.toString()}\n" +
                 "     got: ${result}", pattern.matcher(result).matches())

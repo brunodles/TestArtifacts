f28079a5188c914519bbd8e4bfaf555a42a313ca
Bruno de Lima <dlimaun@gmail.com>
2018-01-23 23:04:58 -0200

    Create GradleRunnerExtension

---
+++ src/test/groovy/com/brunodles/testing/GradleRunnerExtension.groovy
@@ -0,0 +1,14 @@
+package com.brunodles.testing
+
+import org.gradle.testkit.runner.GradleRunner
+
+class GradleRunnerExtension {
+
+    static GradleRunner withJacoco(GradleRunner runner) {
+        def resource = TestResourceReader.readResource('testkit-gradle.properties')
+        def file = new File(runner.projectDir, "gradle.properties")
+        file.createNewFile()
+        file << resource
+        return runner
+    }
+}

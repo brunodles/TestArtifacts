6ee7926a8e8637006c1a2a40e649aaf759e3c519
Bruno de Lima <dlimaun@gmail.com>
2018-01-25 14:28:07 -0200

    Create JacocoXmlTest

---
+++ src/test/groovy/com/brunodles/testartifacts/task/MergeTestArtifactsTest/JacocoXmlTest.groovy
@@ -0,0 +1,57 @@
+package com.brunodles.testartifacts.task.MergeTestArtifactsTest
+
+import com.brunodles.testing.Assertions
+import com.brunodles.testing.TestResourceReader
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4.class)
+class JacocoXmlTest {
+
+    private static final
+    def JACOCO_FILE = TestResourceReader.readResource("MergeTestArtifacts/jacocoxml")
+
+    @Rule
+    public TemporaryFolder testProjectDir = new TemporaryFolder()
+    private File buildFile
+
+    @Before
+    void setupProject() {
+        buildFile = testProjectDir.newFile('build.gradle')
+        buildFile << """
+            plugins {
+                id 'testartifacts' version '0.1.0'
+            }
+            archiver {
+                projectName = "AnimeWatcher"
+                moduleName = "Explorer"
+                buildNumber = 123
+                files = [
+                        'jacoco' : ['jacocoTestReport.xml']
+                ]
+            }
+        """
+        def buildFolder = testProjectDir.newFolder("build")
+        def file = new File(buildFolder, "jacocoTestReport.xml")
+        file.createNewFile()
+        file << JACOCO_FILE
+    }
+
+    @Test
+    void shouldAddJacocoCsvData() {
+        GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('mergeTestArtifacts')
+                .withPluginClasspath()
+                .withJacoco()
+                .build()
+        def reports = new File(testProjectDir.root, 'build/reports/uploadReports.json')
+        def expected = TestResourceReader.readResource("MergeTestArtifacts/jacocoxml_output")
+        Assertions.assertMatches(expected, reports.text)
+    }
+}

482b193e8e616396b245573c0c36a5acf7b63448
Bruno de Lima <dlimaun@gmail.com>
2018-01-25 20:02:45 -0200

    Fix StringUtils

--- src/main/groovy/com/brunodles/testartifacts/helpers/StringUtils.groovy
+++ src/main/groovy/com/brunodles/testartifacts/helpers/StringUtils.groovy
@@ -1,18 +1,22 @@
 package com.brunodles.testartifacts.helpers
 
+import java.text.Normalizer
+
 final class StringUtils {
 
     private StringUtils() {
     }
 
-    static String removeEspecialCharacters(String url) {
-        return url.replaceAll("[^\\d\\w]+", "")
+    static String removeEspecialCharacters(String string) {
+        return Normalizer.normalize(string, Normalizer.Form.NFD)
+                .replaceAll("[\\p{InCombiningDiacriticalMarks}]", "")
+                .replaceAll("[^\\d\\w]+", "")
     }
 
     static String underscoreToCamelCase(String underscore) {
         if (!underscore || underscore.isAllWhitespace()) {
             return ''
         }
-        return underscore.replaceAll(/_\w/) { it[1].toUpperCase() }
+        return underscore.replaceAll(/[_ ]\w/) { it[1].toUpperCase() }
     }
 }
